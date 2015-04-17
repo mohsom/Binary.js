d585d59744148a396c5cef117f67c5a089fe87ea
mohsom <volodya.skalskyi@gmail.com>
2015-04-17 19:38:45 +0300

    Coding

--- src/Binary_.js
+++ src/Binary_.js
@@ -14,13 +14,16 @@ Binary.prototype = {
     toBinary: function (int_value) {
         var new_binary='';
         var i=0;
-        while (int_value !== 0) {
+        while (int_value !=0) {
             var r;
             r = int_value % 2;
             int_value /= 2;
-            new_binary[i]= r;
+            new_binary+= r.toString();
             i++;
+            int_value=parseInt(int_value);
         }
+        new_binary+='0';
+        new_binary=new_binary.split("").reverse().join("");
         return new_binary;
     }
 };
