0586b1156d4b25b5001a36578abb32434029c021
mohsom <volodya.skalskyi@gmail.com>
2015-04-17 19:33:39 +0300

    Initial commit.

--- src/Binary_.js
+++ src/Binary_.js
@@ -2,11 +2,28 @@
  * Binary.js- JavaScript library for working with binary numbers.
  */
 //Binary constructor
-function Binary(binary){
-    if(binary) {
+function Binary(binary) {
+    if (binary) {
         this.binary = binary;
     }
-    else{
-        this.binary='';
+    else {
+        this.binary = '';
     }
 }
+Binary.prototype = {
+    toBinary: function (int_value) {
+        var new_binary='';
+        var i=0;
+        while (int_value !== 0) {
+            var r;
+            r = int_value % 2;
+            int_value /= 2;
+            new_binary[i]= r;
+            i++;
+        }
+        return new_binary;
+    }
+};
+var a=new Binary();
+var b=a.toBinary(3);
+console.log(b);
